15896cd60ff212afee7c4c1b511f0d17c32675dc
Nicko <nicko.chamorro@gmail.com>
2022-12-08 01:08:57 -0300

    actualizaciones conexion back

--- src/components/fixture/Fixture.jsx
+++ src/components/fixture/Fixture.jsx
@@ -1,6 +1,19 @@
 import Stage from "./stages/Stage.jsx"
+/* import { ShowAllGames } from "../services/games/ShowGames.js"  */
 
 function Fixture (){
+
+/*     const games = ShowAllGames();
+    
+    const grupos = games.filter( game => game.fase === "Grupos" );
+
+    console.log( grupos.filter( game => game.grupo === "A") );
+
+    console.log( grupos.filter( game => game.grupo === "B") );
+
+    console.log( games.filter( game => game.fase === "Octavos" ) )
+ */
+
     return(
         <>
             <Stage section="GroupStage"/> 
--- src/config/routes/paths.js
+++ src/config/routes/paths.js
@@ -9,3 +9,17 @@ export const PREDICTIONS = '/private/predicciones';
 export const PROFILE = '/private/perfil';
 export const PROFILEUPD = '/private/perfil/:id';
 export const LOGOUT = '/private/cerrarsesion';
+export const URLBASEAPI = 'https://tpreactprodeqatarback-production.up.railway.app/';
+export const URLPATHUSER = 'users/';
+export const URLPATHGAME = 'games/';
+export const URLPATHPREDICT = 'predictions/';
+
+export const AUTH_APP = 'AUTH_APP'; // variable de sesion
+export const AUTH_USER = 'AUTH_USER'; // variable de sesion
+
+export const CONFIGHEADER = {
+    headers:{
+        'Content-Type': 'application/json;charset=UTF-8',
+        'Access-Control-Allow-Origin': '*'
+    }
+  };
