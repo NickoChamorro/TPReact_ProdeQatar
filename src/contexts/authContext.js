8a0a5ebf2ec671c449e24a8c63e90475087ef802
Nicko <nicko.chamorro@gmail.com>
2022-12-11 20:29:21 -0300

    Actualizacion login

--- src/contexts/authContext.js
+++ src/contexts/authContext.js
@@ -8,39 +8,38 @@ const url = `${URLBASEAPI}${URLPATHUSER}`;
 export const AuthContext = createContext();
 
 function AuthContextProvider({children}){
+
     const [isAuthenticated, setIsAuthenticated] = useState(window.localStorage.getItem(AUTH_APP) ?? false);
     const [idUser, setIdUser] = useState(window.localStorage.getItem(AUTH_USER) ?? 0);
 
     // Funcion de logueo, aca deberia ir la logica que controle sobre el back usuario y pass, y lo marque como logueado, 
     // y si hay error mostrarlo, dejando además lo de localStoge
     const login = useCallback((user, pass)=>{
-        try{
-            // Traer id usuario por usuario+pass    
-            const urlComplete = `${url}login/${user}/pass/${pass}`;
-            const getUser = async () =>{
-                const res = await axios.get(urlComplete, CONFIGHEADER);
-                if (res===null || res.data.idUsuario === 0){
-                    alert('Usuario y/o contraseña incorrectos.')
-                    return
-                };
-                setIdUser(res.data.idUsuario);
-                window.localStorage.setItem(AUTH_USER, res.data.idUsuario)
+        
+        // Traer id usuario por usuario+pass    
+        const urlComplete = `${url}login/${user}/pass/${pass}`;
+        const getUser = async () =>{
+            const res = await axios.get(urlComplete, CONFIGHEADER);
+            if (res===null || res.data.idUsuario === 0){
+                alert('Usuario y/o contraseña incorrectos.')
+                return
             };
-            getUser();
-            
-            // Actualizar logueo en base
-            const update = async () =>{
-                await axios.put (url+idUser,{
-                    login:1,
-                }, CONFIGHEADER)
-            };
-            update();
+            setIdUser(res.data.idUsuario);
+            window.localStorage.setItem(AUTH_USER, res.data.idUsuario)
+        };
+        getUser();
+        
+        // Actualizar logueo en base
+        const update = async () =>{
+            await axios.put (url+idUser,{
+                login:1,
+            }, CONFIGHEADER)
+        };
+        update();
 
-            window.localStorage.setItem(AUTH_APP, true);
-            setIsAuthenticated(true);
-        } catch (error){
-            const errorMsg = error.message
-        }    
+        window.localStorage.setItem(AUTH_APP, true);
+        setIsAuthenticated(true);
+
     }, [idUser]);
 
     // Funcion de deslogueo
