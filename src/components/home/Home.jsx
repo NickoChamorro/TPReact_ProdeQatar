1392c6c743685b0485a0b40e86018bd2ff491b0a
camila <camilamaita07@gmail.com>
2022-12-13 02:40:19 -0300

    agrego div para resolver img Copa

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,4 @@
-import { Myh1, Info, ContainerButton, ImInicio, DivImg} from "./HomeStyle";
+import { Myh1, Info, ContainerButton, ImgCopa, DivImg ,DivTotal, DivTexto} from "./HomeStyle";
 import { useContextProvider } from "../../contexts/authContext";
 import { useState } from "react";
 import Button from "../commons/button/Button.jsx";
@@ -15,33 +15,38 @@ function Home(){
     }
     return (
         <>  
-            <DivImg>
-                <ImInicio src= {imgInicio} alt="img_inicio"/>
-            </DivImg>
-            <Myh1>Prode Qatar 2022</Myh1>
-            <Info>
-                Prode Qatar 2022 es un juego online de pronóstico deportivo que incentiva la competencia sana. Diviértete con tus amigos, compañeros de trabajo o estudio, a través de la predicción de los resultados de los partidos del mundial de manera totalmente gratuita.
-            </Info>
-            
-            <Myh1>¿Cómo realizar mis predicciones?</Myh1>
-            <Info>
-                Los partidos van a estar disponibles por etapas: fase de grupos, luego octavos, cuartos, semifinales y, por último, tercer y cuarto puesto y final. Las predicciones deben realizarse antes de comenzar los partidos, ese es el tiempo límite para realizar el pronóstico.
-                Por cada partido hay 2 botones de check, 1 por cada equipo. Si nuestro pronóstico es que va a ganar un equipo en particular seleccionamos el check de ese equipo, y si creemos que va a ser un empate, seleccionamos los 2. 
-                De octavos en adelante no hay empates, se toma en cuenta el resultado final por más que haya empate en el tiempo reglamentario y vayan a alargue o penales.
-            </Info>
-            <Myh1>Puntuación</Myh1>
-            <Info>
-                Cada partido acertado suma 1 punto. El puntaje se puede observar en el perfil del usuario o en el menú ranking con el podio de usuarios y los puntos acumulados.
-            </Info>
-            <Myh1>¿Cómo empezar a jugar?</Myh1>
-            <Info>
-                Empezar a jugar requiere de sólo 2 pasos: registrarse al sitio (si aún no tenés una cuenta creada) y luego, cargar tus pronósticos. Así de fácil. Así de sencillo. Así de rápido.
-            </Info>
-            
-            <ContainerButton>
-                <Button action={botonShowMobileMenu} string={isAuthenticated?'Mi Perfil':'Jugar ahora'} route='/public/iniciosesion'/>
-            </ContainerButton>  
-             
+            <DivTotal>            
+                <DivTexto>
+                    <Myh1>Prode Qatar 2022</Myh1>
+                    <Info>
+                        Prode Qatar 2022 es un juego online de pronóstico deportivo que incentiva la competencia sana. Diviértete con tus amigos, compañeros de trabajo o estudio, a través de la predicción de los resultados de los partidos del mundial de manera totalmente gratuita.
+                    </Info>
+
+                    <Myh1>¿Cómo realizar mis predicciones?</Myh1>
+                    <Info>
+                        Los partidos van a estar disponibles por etapas: fase de grupos, luego octavos, cuartos, semifinales y, por último, tercer y cuarto puesto y final. Las predicciones deben realizarse antes de comenzar los partidos, ese es el tiempo límite para realizar el pronóstico.
+                        Por cada partido hay 2 botones de check, 1 por cada equipo. Si nuestro pronóstico es que va a ganar un equipo en particular seleccionamos el check de ese equipo, y si creemos que va a ser un empate, seleccionamos los 2. 
+                        De octavos en adelante no hay empates, se toma en cuenta el resultado final por más que haya empate en el tiempo reglamentario y vayan a alargue o penales.
+                    </Info>
+                    <Myh1>Puntuación</Myh1>
+                    <Info>
+                        Cada partido acertado suma 1 punto. El puntaje se puede observar en el perfil del usuario o en el menú ranking con el podio de usuarios y los puntos acumulados.
+                    </Info>
+                    <Myh1>¿Cómo empezar a jugar?</Myh1>
+                    <Info>
+                        Empezar a jugar requiere de sólo 2 pasos: registrarse al sitio (si aún no tenés una cuenta creada) y luego, cargar tus pronósticos. Así de fácil. Así de sencillo. Así de rápido.
+                    </Info>
+
+                    <ContainerButton>
+                        <Button action={botonShowMobileMenu} string={isAuthenticated?'Mi Perfil':'Jugar ahora'} route='/public/iniciosesion'/>
+                    </ContainerButton>  
+                </DivTexto>
+
+                <DivImg>
+                    <ImgCopa src= {imgInicio} alt="img_inicio"/>
+                </DivImg>
+            </DivTotal>
+
         </>
     )
 }
--- src/components/home/HomeStyle.js
+++ src/components/home/HomeStyle.js
@@ -1,63 +1,48 @@
 import styled from "styled-components";
 
-
-export const DivImg = styled.div`
+export const DivTotal = styled.div`
     width: 100%;
     display: flex;
-    position: relative;
-    justify-content: right;
-    backdrop-filter: initial;
-    border-top: 20px solid transparent;
-    @media (max-width: 1094px) {
-        justify-content: center;
-        background-color: rgba(0,0,0,0.5);
-        border-left: 180px solid rgba(255,255,255,0.5) ;
-        border-right: 180px solid rgba(255,255,255,0.5);
-        /* border-bottom: 20px solid transparent; */
-        border-top: none;
+    margin-top: 1rem;
+    margin-bottom: 2rem;
+    @media (max-width: 684px) {
+        flex-direction: column-reverse;
+        align-items: center;
+    } 
+    
+
+`;
+export const DivTexto = styled.div`
+    margin-left: 3rem;
+    text-align: left;
+    width: 80%;
+    @media (max-width: 684px) {
+        text-align: center;
+        margin-right: 3rem;
 
     } 
-    @media (max-width: 750px) {
-        border-left: 110px solid rgba(255,255,255,0.5) ;
-        border-right: 110px solid rgba(255,255,255,0.5);
+
+`;
+export const DivImg = styled.div`
+    @media (max-width: 826px) {
+        width: 40%;
     } 
-   
+    @media (max-width: 684px) {
+        width: 20%;
+    } 
 `;
 
-
-export const ImInicio = styled.img`
-    max-width: 23%;
-    /* height: auto; */
-    position: absolute;
-    margin: 4rem 2rem 0 2rem;
-    /* background-color: rgba(0,0,0,0.5); */
-    
-    @media (max-width: 1094px) {
-        position: relative;
-        margin: 1rem 0 1rem 0;
-        max-width: 24%;
-        /* background-color: transparent; */
-
-    }
-    @media (max-width: 570px) {
-        max-width: 30%;
-    }
+export const ImgCopa = styled.img`
+    width: 100%;
+    height: auto;
+    margin-top: 1rem;
 `;
 
 export const Myh1 = styled.h1`
     color: ${(props)=>props.theme.colors.whiteBg};
     font-weight: bold;
     font-size: 150%;
-    display: flex;
-    justify-content: left;
-    min-width: 16rem;
-    margin-left: 3rem;
-    margin-right: 3rem;
-    @media (max-width: 1094px) {
-    justify-content: center;
-
-
-} 
+    
     @media (max-width: 900px) {
         font-size:130%;
     }  
@@ -65,22 +50,7 @@ export const Myh1 = styled.h1`
 export const Info = styled.p`
     color: ${(props)=>props.theme.colors.whiteBg};
     font-size: 90%;
-    display: flex;
-    justify-content: left;
-    max-width: 55%;
-    margin-left: 3rem;
     
-    @media (max-width: 1094px) {
-        max-width: 80%;
-       
-    }  
-    @media (max-width: 1094px) {
-        justify-content: center;
-        max-width: 100%;
-        text-align: center;
-        margin-right: 3rem;
-       
-    }
     @media (max-width: 900px) {
         font-size:80%;
     }  
@@ -88,22 +58,14 @@ export const Info = styled.p`
 
 export const ContainerButton = styled.div`
 display: flex;
-justify-content: left;
 margin-top: 1rem;
-margin-left: 3rem;
-margin-right: 3rem;
-margin-bottom: 3rem;
 transition: all .6s ease;
 a:hover{
     color: ${(props)=>props.theme.colors.whiteBg};
 }
-@media (max-width: 1094px) {
+@media (max-width: 684px) {
     justify-content: center;
-
 
 }
    
-@media (max-width: 900px) {
-    margin-top: 2rem;
-}  
-`
+`;
