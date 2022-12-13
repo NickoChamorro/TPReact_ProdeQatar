8886a75f1b86e5ee5891a2dfef4aecda45343bcc
Nicko <nicko.chamorro@gmail.com>
2022-12-13 04:09:00 -0300

    Actualizacion ranking

--- src/components/ranking/ranking.jsx
+++ src/components/ranking/ranking.jsx
@@ -1,57 +1,81 @@
-import React from "react";
-import Marquee from "./Marquee";
-import CarteleraGanador from "./CarteleraGanador";
-import posiciones from "./posiciones.json";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Marquee from "./Marquee.js";
+import CarteleraGanador from "./CarteleraGanador.js";
 import "./ranking.css";
+import { CONFIGHEADER, URLBASEAPI, URLPATHUSER } from "../../config/routes/paths.js";
 
 function Ranking(){
 
+    const url = `${URLBASEAPI}${URLPATHUSER}`;
+    const [posiciones, setPosiciones] = useState([]);
+    const [lider, setLider] = useState('');
+
+    useEffect(()=>{ 
+        async function getUsers(){
+            const res = await axios.get(url, CONFIGHEADER);
+            const resOrdenado = await res.data.sort((a, b) => {
+                if(a.puntos < b.puntos) {
+                  return 1;
+                }
+                if(a.puntos > b.puntos) {
+                  return -1;
+                }
+                return 0;
+            })
+            setPosiciones(resOrdenado);
+            setLider(resOrdenado[0].usuario);
+        }    
+        getUsers()
+    },[url]) 
+
     const encabezadoRanking= {
-        fontSize: "4rem", marginBottom:0   }
+        fontSize: "2rem",
+        marginTop: 0, 
+        marginBottom:0   
+    }
 
-   const mainRanking= {
-    margin:0,
-    textAlign: "center", 
-    color: 'goldenrod',
-    display:"flex",
-    flexDirection:"column",
-    alignItems:"center",
-    justifyContent:"center",
-}
-const tablaRanking= {width:"50%",
-maxWidth:"700px",
-border:"3px solid white",
-color:"white",
-borderCollapse:"collapse",
-fontSize: "16px",
- }
-
-    
+    const mainRanking= {
+        margin:0,
+        textAlign: "center", 
+        color: 'goldenrod',
+        display:"flex",
+        flexDirection:"column",
+        alignItems:"center",
+        justifyContent:"center",
+    }
+    const tablaRanking= {width:"50%",
+        maxWidth:"700px",
+        border:"3px solid white",
+        color:"white",
+        borderCollapse:"collapse",
+        fontSize: "16px",
+    }
 
     return (
     <main style={mainRanking}>
     <header>
     <h1 style={encabezadoRanking}>Ranking</h1>
-    <Marquee string={`${posiciones[0].nombre} lidera la tabla`}/>
+    <Marquee string={`${lider} lidera la tabla`}/>
     </header>
-    <CarteleraGanador />
+    <CarteleraGanador lider={lider}/> {/* posicion1={posiciones[0].usuario} posicion2={posiciones[1].usuario} posicion3={posiciones[2].usuario} */}
     <table style={tablaRanking}>
         <thead>
         <tr>
             <th>Pos.</th>
-            <th>Nombre</th>
+            <th>Usuario</th>
             <th>Puntaje</th>
         </tr>
         </thead>
         <tbody style={{background:"rgba(149,20,52,0.25)"}}>
-        { posiciones.map((fila)=>
-            <tr key={fila.nombre}>
-                <td style={{padding:"0.75rem 0 0.35rem" }}>{fila.posicion}</td>
-                <td style={{padding:"0.75rem 0 0.35rem" }}>{fila.nombre}</td>
+        { posiciones.map((fila, index)=>
+            <tr key={fila.idUsuario}>
+                <td style={{padding:"0.75rem 0 0.35rem" }}>{index+1}</td>
+                <td style={{padding:"0.75rem 0 0.35rem" }}>{fila.usuario}</td>
                 <td style={{padding:"0.75rem 0 0.35rem" }}>{fila.puntos}</td>
             </tr>
             )
-        }
+        } 
         </tbody>
 
     </table>
